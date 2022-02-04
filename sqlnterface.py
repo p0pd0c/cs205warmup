4116aeb6caa7bad295a411f8bef5d93a4a57286e
Jared DiScipio <jdiscipi@uvm.edu>
2022-02-04 18:42:44 -0500

    fix hidden space in director record add kwargs to queries

--- sqlnterface.py
+++ sqlnterface.py
@@ -9,11 +9,13 @@ def main():
     # db team maps the command to the sql equivalent, runs the query and returns the data to the parser team
     # language is subject to change this is just an example
     interface = Interface(r"IM.db")
-    print("\n".join(["".join(str(x)) for x in interface.select("get highest grossing movie for all directors")]))
-    print("*"*80)
-    print("\n".join(["".join(str(x)) for x in interface.select("get total budget for all directors")]))
-    print("*"*80)
-    print("\n".join(["".join(str(x)) for x in interface.select("get all movies by director", id=3)]))
+    #print("\n".join(["".join(str(x)) for x in interface.select("get highest grossing movie for all directors")]))
+    #print("*"*80)
+    #print("\n".join(["".join(str(x)) for x in interface.select("get total budget for all directors")]))
+    #print("*"*80)
+    #print("\n".join(["".join(str(x)) for x in interface.select("get all movies by director", id=3)]))
+    #print("*"*80)
+    print("\n".join(["".join(str(x)) for x in interface.select("net profit", director="Francis Ford Coppola")]))
     interface.close_connection()
 
 
@@ -63,6 +65,21 @@ class Interface:
             curr = self.conn.cursor()
             curr.execute(sql, (str(kwargs["id"])))
             return curr.fetchall()
+        elif command == "net profit":
+            if kwargs["director"]:
+                sql = """
+                    select sum(gross - budget) as "Net Profit" from movies
+                    join directors on movies.director_id = directors.id
+                    where first_name = ? and last_name = ?
+                """
+                curr = self.conn.cursor()
+                name = kwargs["director"].split(" ")
+                if len(name) > 2:
+                    name = [" ".join([name[0], name[1]]).strip(), name[2]]  # strip may be redundant
+                else:
+                    name = [name[0], name[1]]
+                curr.execute(sql, ('Francis Ford', 'Coppola'))
+                return curr.fetchall()
 
         if command is not None:
             curr = self.conn.cursor()
